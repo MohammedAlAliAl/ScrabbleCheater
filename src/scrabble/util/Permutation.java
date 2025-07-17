dc3812cfd5e0d2a2cbfc1a68e92145772b1f4c6e
mohammed <m.alali14@icloud.com>
2025-07-17 12:13:45 +0200

    Implemented Permutation logic and CLI interface

--- src/scrabble/util/Permutation.java
+++ src/scrabble/util/Permutation.java
@@ -1,21 +1,26 @@
 package scrabble.util;
-
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Permutation {
+	private final String word;
 
 	public Permutation(String word) {
+		this.word = word;
 	}
 
 	@Override
 	public int hashCode() {
-		// TBD: implement this method
-		return 0;
+		return Objects.hash(getNormalized());
+
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		//TBD: Implement equals method
-		return false;
+		if (this == obj) return true;
+		if (!(obj instanceof Permutation)) return false;
+		Permutation other = (Permutation) obj;
+		return this.getNormalized().equals(other.getNormalized());
 	}
 
 	@Override
@@ -24,18 +29,19 @@ public class Permutation {
 	}
 
 	public String getNormalized() {
-		// TBD: implement this method
-		return null;
+		char[] chars = word.toCharArray();
+		Arrays.sort(chars);
+		return new String(chars);
+
 	}
 
 	public String getWord() {
 		// TBD: implement this method
-		return null;
+		return word;
 	}
 
 	public int length() {
-		// TBD: implement this method
-		return 0;
+		return word.length();
 	}
 
 }
